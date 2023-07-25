bb01a9d577c57ce1617877b842848f8082f59deb
LailaEhab <lailaihab24@gmail.com>
2023-07-25 15:42:53 -0700

    d

--- number.c
+++ number.c
@@ -25,7 +25,6 @@ char *convert(long int num, int base, int flags, params_t *params)
 	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
 	ptr = &buffer[49];
 	*ptr = '\0';
-
 	do {
 		*--ptr = array[n % base];
 		n /= base;
@@ -45,7 +44,6 @@ char *convert(long int num, int base, int flags, params_t *params)
 int print_unsigned(va_list ap, params_t *params)
 {
 	unsigned long 1;
-
 	if (params->1_modifier)
 		l = (unsigned long)va_arg(ap, unsigned long);
 	else if (params->h_modifier)
@@ -66,7 +64,6 @@ int print_address(va_list ap, params_t *params)
 {
 	unsigned long int n = va_arg(ap, unsigned long int);
 	char *str;
-
 	if (!n)
 		return (_puts("(nil)"));
 	str = convert(n, 16, CONVERT_UNSIGNED | CONVER_LOWERCASE, params);
